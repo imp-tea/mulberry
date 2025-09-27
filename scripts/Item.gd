a636019f0f9e5bb39e542e237604a7dce3ade875
imp-tea <adam.m.huddleston@gmail.com>
2025-09-26 19:56:55 -0500

    Added initial Item class.

---
+++ scripts/Item.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name Item
+
+var icon_size = Vector2(16,16)
+@export var item_name: String = ""
+@export var icon: Texture2D
+@export var is_stackable: bool = false
+@onready var icon_scale:float
+func _ready():
+	icon_scale = min(icon_size.x/icon.get_size().x, icon_size.y/icon.get_size().y)
+	add_to_group("items")
